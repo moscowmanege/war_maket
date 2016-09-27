2ffd1811b08cf2a246177bfe25fcf9e6bb4f8173
ponyspy <desade4me@gmail.com>
2016-09-27 19:29:24 +0300

    init

--- js/main.js
+++ js/main.js
@@ -2,14 +2,6 @@ $(function() {
 
 	// Swiper
 
-	var swiperV = new Swiper('.swiper-container-v', {
-		pagination: '.swiper-pagination-v',
-		paginationClickable: true,
-		direction: 'vertical',
-		keyboardControl: true,
-		touchRatio: 0
-	});
-
 	var swiperH = new Swiper('.swiper-container-h', {
 		pagination: '.swiper-pagination-h',
 		paginationClickable: true,
@@ -24,7 +16,7 @@ $(function() {
 
 	// Leaflet
 
-	var map = L.map('plan_map', { zoomControl: false, attributionControl: false }).setView([0, 0], 2);
+	var map = L.map('plan_map', { scrollWheelZoom: false, zoomControl: false, attributionControl: false }).setView([0, 0], 2);
 
 	L.tileLayer('img/tiles/{z}/image_tile_{y}_{x}.jpg', {
 		minZoom: 1,
@@ -38,33 +30,5 @@ $(function() {
 
 	L.control.zoom({ position: 'bottomleft' }).addTo(map);
 
-
-
-	// Interact
-
-	interact('.navigate_gap', { context: document }).draggable({
-		axis: 'y',
-		inertia: true,
-		restrict: {
-			restriction: 'parent',
-			endOnly: true,
-			elementRect: { left: 0, right: 0, top: 0, bottom: 1 },
-		},
-		onmove: function(event) {
-			var $target = $(event.target);
-			var y = (parseFloat($target.attr('data-y')) || 0) + event.dy;
-
-			var delta_speed = Math.abs(y) / event.speed > 0.35;
-
-			if (y >= 30 && delta_speed) {
-				swiperV.slideNext();
-			} else if (y <= -30 && delta_speed) {
-				swiperV.slidePrev();
-			}
-
-			$target.css('transform', 'translateY(' + y + 'px)');
-			$target.attr('data-y', y);
-		}
-	});
 
 });
